be05947689a9b9d4cdfcf77ded8a4c44357c78ac
DEC <DEC@DESKTOP-E8QCUQ8>
2024-11-18 21:04:56 +0100

    Add project files.

---
+++ MyProject/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Npgsql;
+using System;
+
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        string connectionString = ("Server=localhost;Password=lamis;Database=lamisplus; User Id=postgres;Port=5432;");
+		string query = "SELECT * from patient_person"
+		try
+		{
+			using (var conn = new NpgsqlConnection(connectionString))
+			{
+				conn.Open();
+				Console.WriteLine("Successfully Connected to EMR");
+
+			}
+		}
+		catch (Exception connException)
+		{
+
+			Console.WriteLine($"A critical Error occur check Details:{connException}");
+		}
+    }
+}
+
+
